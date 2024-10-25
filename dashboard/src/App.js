8dc5e15b176208656a88b06b9b2ad8010d26c132
Pratik1657 <pratiktillekar57@gmail.com>
2024-10-25 09:21:06 +0530

    project initiated

---
+++ dashboard/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>This is a simple React app.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
